273df5fd1e0fcaed81e619e46bb38e50a62df305
pooochoo <javiergarciasantiago90@gmail.com>
2025-05-21 10:54:44 +0200

    Añadimiento de codigo JavaScript

--- proyecto_CSS_Javier/landing.js
+++ proyecto_CSS_Javier/landing.js
@@ -1,14 +1,11 @@
-document.addEventListener("DOMContentLoaded", mostrar )
+document.getElementById("btn").addEventListener("click", mostrar )
 
 function mostrar() {
-  const toggleBtn = document.getElementById("btn");
   const planes = document.getElementById("planes");
 
-  toggleBtn.addEventListener("click", function () {
     if (planes.style.display === "none") {
       planes.style.display = "flex";
     } else {
       planes.style.display = "none";
     }
-  });
 };
